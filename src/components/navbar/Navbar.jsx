b80ee4274bbf8bee0547b8e50e9339def1ecac7c
blade <nkwetakembrunodud9@gmail.com>
2022-12-27 16:38:48 +0100

    first conit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import logo from "../images/logo.svg"
+import hamburger from "../images/icon-menu.svg"
+import Button from "../button/Button"
+
+
+
+
+
+const Navbar = () => {
+
+  // usestae to hide and show components based on button click 
+  const [show, setShow] = useState();
+  const display = show ?"show":"navlinks-mob";
+  const toggleshow=()=>{
+    setShow(!show);
+  }
+  return (
+    <div>
+
+        <nav className="mobile">   
+          <div className="mob-left"><img src={logo} alt="logo" className="logo" /></div>
+          <div className="mob-right">
+            <div onClick={toggleshow}> <img src={hamburger} alt="menu-btn"  className="haburger"/></div>
+          <ul className={`navlinks-mob ${display}`}>
+           <li className="navlink"><a href="/">Features</a> </li>   
+           <li className="navlink"><a href="/"> Pricing</a> </li>  
+           <li className="navlink"><a href="/"> Resources</a> </li> <hr />
+           <li className="navlink"><a href="/">Login </a> </li>
+           {/* na yi this wey no di chane when reused */}
+            <Button style={{  marginTop:"2em" , width:"90%",borderRadius:"30px", fontSize:"20px",fontWeight:"bold" ,padding:"0.5em 0em"}} text="Sign Up"/>
+            </ul>
+            </div>
+      </nav>
+        <nav className="desktop">  
+                <nav className="des-left">
+          <div className="left-logo"><img src={logo} alt="logo" className="logo" /></div>
+            <div className="links">
+              <ul className="navlinks  desktop">
+            <li className="navlink"><a href="/">Features</a> </li>   
+            <li className="navlink"><a href="/"> Pricing</a> </li>  
+            <li className="navlink"><a href="/"> Resources</a> </li> 
+              </ul>
+              </div>
+          </nav>
+          <div className="des-right">
+            <div className="b1">login</div>
+            <div className="b2">
+              <Button style= {{background:"green" }} text="login" />
+            </div>
+          </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
