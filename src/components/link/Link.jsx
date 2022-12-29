9801f75bbff9580f0c92eab09cfaf128cfe6094a
blade <nkwetakembrunodud9@gmail.com>
2022-12-30 00:36:41 +0100

    changes made

--- src/components/link/Link.jsx
+++ src/components/link/Link.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import Button from '../button/Button'
-import bgshorten from "../images/bg-shorten-mobile.svg"
-import bgshorten1 from "../images/bg-shorten-desktop.svg"
 
 const Link = () => {
+
+  const [name,setname] =useState(" ")
+ 
+  const handleChange =(e)=>{
+    let inputVal = e.target.value;
+    setname(inputVal)
+  }
+      const handleSubmit=(e)=>{
+        e.preventDefault();
+        e.target.reset();
+    }
+  
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+    '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+
+const [users, setUsers] = useState([])
+
+const fetchUserData = () => {
+  fetch(`https://api.shrtco.de/v2/shorten?url=${name}`)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setUsers(data)
+      if(urlPattern.test(name)){
+        console.log(" bro its true");
+        console.log(name);
+        console.log(data.result.full_short_link  ); 
+      }
+      else{
+        console.log("fork you  fake mehn");
+      }
+    })
+
+}
+useEffect(() => {
+  fetchUserData()
+},[])
+// the end of the api fetch the beginnning of parametrs analysis
+
   return (
     <div >
-         
-        <section className="main-link">
-            <div className="link-input"> <input type="text" className='link'  placeholder='Shorten a link here '/>  </div>
-          
+   
+          <form action="" className="form main-link" onSubmit={handleSubmit}>  
+            <div className="link-input"> <input type="text" className='link' onChange={handleChange} 
+             placeholder='Shorten a link here' />  </div>
             <div className="link-btn "> 
             <Button text="Shorten it !" style={{width:"100%" ,fontSize:"18px",padding:"0.7em",fontWeight:"bold", borderRadius:"7px" }}  /> 
             </div>
-         
-        </section>
+            </form>
        
         <section className="statistics">
           <h1 className="stats-header">Advanced Statistics</h1>
@@ -22,7 +63,6 @@ const Link = () => {
             advanced statistics dashboard.</p>
         </section>
        
-      
     </div>
   )
 }
